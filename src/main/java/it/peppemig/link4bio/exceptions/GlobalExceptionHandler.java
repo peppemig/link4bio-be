74af334f6123e8713ef3f21ed24236d369c703cc
giuseppe.migliozzi@outlook.it <giuseppe.migliozzi@outlook.it>
2023-12-30 19:31:56 +0100

    entity not found exception handling

--- src/main/java/it/peppemig/link4bio/exceptions/GlobalExceptionHandler.java
+++ src/main/java/it/peppemig/link4bio/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package it.peppemig.link4bio.exceptions;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,5 +13,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UnauthorizedException.class)
     public ResponseEntity<Object> handleUnauthorizedException(UnauthorizedException ex, WebRequest request) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
